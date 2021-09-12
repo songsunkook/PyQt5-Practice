9155170913548075838af0864732c0715d3d1faa
songsunkook <songsunkook@gmail.com>
2021-09-12 07:05:30 +0000

    why not running this code..

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from Color import Color
+from PyQt5.QtWidgets import *
 
-class MyApp(QWidget):
+class MyApp(QMainWindow):
   def __init__(self):
     super().__init__()
     self.initUI()
   
   def initUI(self):
+    btn1 = QPushButton('quit',self)
+    btn1.resize(btn1.sizeHint())
+    btn1.move(50,50)
+    btn2 = QPushButton('ddd',self)
+    btn2.resize(btn2.sizeHint())
+    btn2.move(100,50)
+ #   layout = QVBoxLayout()
+ #   layout.addWidget(QLabel('red',self))
+ #   layout.addWidget(QLabel('blue',self))
+ #   layout.addWidget(QLabel('green',self))
+ #   layout.addWidget(Color('red'))
+ #   layout.addWidget(Color('green'))
+ #   layout.addWidget(Color('blue'))
+ #   layout.addWidget(btn1)
+ #   layout.addWidget(btn2)
+ #   widget=QWidget()
+ #   widget.setLayout(layout)
+ #   self.setCentralWidget(widget)
+
     self.setWindowTitle("ddd")
     self.move(300,300)
     self.resize(400,200)
